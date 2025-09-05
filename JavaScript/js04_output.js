3b08301d6976603fe4c54e6e300b7e2d668d8778
Afsar Tanvir <afsartanvir805@gmail.com>
2025-09-05 19:03:20 +0600

    JavaScript Learning 1st Commit

---
+++ JavaScript/js04_output.js
@@ -0,0 +1,36 @@
+function testOutputs() {
+  // 1. innerHTML → includes HTML tags
+  document.getElementById("p1").innerHTML = "This is <b>innerHTML</b>";
+
+  // 2. innerText → only plain text (ignores HTML tags)
+  document.getElementById("title").innerText =
+    "This is innerText <b>ignored</b>";
+
+  // 3. document.write() → writes directly to the page (overwrites if used after load)
+  document.write("This is document.write() - it can overwrite the whole page!");
+
+  // 4. alert() → popup box
+  alert("This is an alert box!");
+
+  // 5. console.log() → outputs to browser console (F12 > Console tab)
+  console.log("This is console.log() output");
+
+  // 6. window.print() → opens print dialog
+  // (uncomment if you want to test printing)
+  // window.print();
+}
+
+// for changing the paragraph
+function changePara() {
+  document.getElementById("p1").innerHTML = "See it change <b>Paragraph</b>.";
+}
+
+// for alert Message
+function alertButton() {
+  alert("Why did you click me?");
+}
+
+// for print the document
+function printButton(){
+    window.print();
+}
---
+++ JavaScript/learn_js_basic01.js
@@ -0,0 +1,56 @@
+// let str = "My name is Afsar Tanvir.";
+// console.log(str);
+
+// str = str.replace("name", "Name");
+// console.log(str);
+
+// str = str.replace('a', '@');
+// console.log(str);
+
+// str = str.replaceAll('a', '*');
+// console.log(str);
+
+// let strLength = str.length;
+// console.log(strLength);
+
+// //arrow function
+// const arrowMul = (a, b) => {
+//     return a*b;
+// }
+// console.log(arrowMul(3, 4));
+
+//
+let nums = [2, 3, 4, 5, 6];
+// nums.forEach((num) => {
+//     console.log(num**2);
+// });
+
+// let calcSquare = (num) => {
+//     console.log(num * num);
+// };
+// nums.forEach(calcSquare);
+
+// Map
+// let newArr = nums.map((val) => {
+//   return val * 2;
+// });
+
+// console.log(newArr);
+
+// // Filter
+// let evenArr = nums.filter((val) => {
+//   return val % 2 == 0;
+// });
+
+// console.log(evenArr);
+
+// // Reduce -> it return a single value
+// let sum = nums.reduce((sum, cur) => {
+//   return sum + cur;
+// });
+// console.log(sum);
+
+
+// Join
+let joinArr = nums.join(" Fish,\n");
+console.log(joinArr);
